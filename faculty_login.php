8ba3ed07c32d0bef149304d071544a9e057ee3bb
Md. Mehedi Hasan Shishir <74834693+notthedexter@users.noreply.github.com>
2023-12-04 14:46:02 +0600

    Update faculty_login.php

--- faculty_login.php
+++ faculty_login.php
@@ -1,18 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login Page</title>
-</head>
-<body>
-    <form action="faculty_lgcheck.php" method="post">
-        <label for="faculty_id">Faculty ID:</label><br>
-        <input type="text" id="faculty_id" name="faculty_id"><br>
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email"><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br>
-        <input type="submit" value="Login">
-    </form>
-    <button onclick="window.location.href='faculty_signup.php'">Register</button>
-</body>
-</html>
+

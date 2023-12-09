e22f0346c2ce99fdc8f0e4eb3a3090babacb2b7e
Sowhardo221 <105670032+Sowhardo12@users.noreply.github.com>
2023-12-09 20:36:47 +0600

    minor changes ()

--- student_login.php
+++ student_login.php
@@ -34,6 +34,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Student Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+        
     <style>
         body {  
             background: #e4e9f7; /* White background for the body */
@@ -108,7 +110,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="mb-3">
                     <input type="password" class="form-control" id="password" name="password" placeholder="Password">
                 </div>
-                <button type="submit" class="btn btn-blue">Submit</button>
+                <div class="field button">
+                        <input type="submit" class="btn" name="submit" value="Submit" required>
+                </div>
+                <div class="links">
+                        Already a member? <a href="student_login.php">Log In</a>
+                </div>
             </form>
         </div>
     </div>

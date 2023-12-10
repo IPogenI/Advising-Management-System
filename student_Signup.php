6e3c74da0e8e0fef109313978db2344c429d8d52
Iit01b <irfanulislam2001@gmail.com>
2023-12-10 15:24:36 +0600

    cleaning up folder

--- student_Signup.php
+++ student_Signup.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./styles/style.css">
     <title>Student SignUp</title>
 </head>
 <body>
@@ -38,9 +38,9 @@
                 echo "<div class='message'>
                         <p>Registration successfully!</p>
                     </div> <br>";
-                echo "<a href='index.php'><button class='btn'>Login Now</button>";
+                echo "<a href='student_login.php'><button class='btn'>Login Now</button>";
                 
-                header("location: student_login.php");
+                //header("location: student_login.php");
 
             }
 
